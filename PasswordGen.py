7d1a754d3637dd793e455ae192bc2738ef7f66c6
l0wGK <87951785+l0wGK@users.noreply.github.com>
2021-07-27 03:53:05 +0200

    Add files via upload

--- PasswordGen.py
+++ PasswordGen.py
@@ -2,10 +2,14 @@ import passgen
 
 p = passgen
 
-while True:
+def gen():
     num = int(input("Password length: "))
     if num >= 51 or num <= 7:
         print("Password length must be between 8-50 characters")
+        return
     password = p.generate(num)
     p.copy(password)
     print("Password copied to clipboard")
+
+while True:
+    gen()
